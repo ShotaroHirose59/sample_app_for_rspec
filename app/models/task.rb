ff750262b61841e26add512cf3d17f055e4cff33
Shotaro Hirose <shotaro199959@icloud.com>
2020-01-21 19:31:02 +0900

    [fix] belongs_toなどの他モデルとの関連性はvalidationよりも先に定義

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true, uniqueness: true
   validates :status, presence: true
   enum status: { todo: 0, doing: 1, done: 2 }
-  belongs_to :user
 end
