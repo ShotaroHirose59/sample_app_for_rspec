a0efd53ffcad28fad661d9333ecf7d2fbe29885b
Shotaro Hirose <shotaro199959@icloud.com>
2020-01-21 20:07:57 +0900

    to_be validを使ってtask.valid?の部分でもexpectを使う

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,20 +4,20 @@ RSpec.describe Task, type: :model do
   describe 'validation' do
     it 'titleが空の場合は無効' do
       task = build(:task, title: nil)
-      task.valid?
+      expect(task).not_to be_valid
       expect(task.errors[:title]).to include("can't be blank")
     end
 
     it '重複したタイトルの場合は無効' do
       FactoryBot.create(:task, title: 'duplicate_task')
       task = build(:task, title: 'duplicate_task')
-      task.valid?
+      expect(task).not_to be_valid
       expect(task.errors[:title]).to include("has already been taken")
     end
 
     it 'statusが空の場合は無効' do
       task = build(:task, status: nil)
-      task.valid?
+      expect(task).not_to be_valid
       expect(task.errors[:status]).to include("can't be blank")
     end
   end
