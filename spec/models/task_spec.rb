618f0e0889ed8c5c6563f2ae1ed65d1c114bded8
Shotaro Hirose <shotaro199959@icloud.com>
2020-01-21 21:01:01 +0900

    重複データを扱う変数の名前をduplicate_taskにする

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+
   describe 'validation' do
     it 'titleが空の場合は無効' do
       task = build(:task, title: nil)
@@ -9,10 +10,9 @@ RSpec.describe Task, type: :model do
     end
 
     it '重複したタイトルの場合は無効' do
-      FactoryBot.create(:task, title: 'duplicate_task')
-      task = build(:task, title: 'duplicate_task')
-      expect(task).not_to be_valid
-      expect(task.errors[:title]).to include("has already been taken")
+      task = create(:task)
+      expect(duplicate_task = build(:task)).not_to be_valid
+      expect(duplicate_task.errors[:title]).to include("has already been taken")
     end
 
     it 'statusが空の場合は無効' do
