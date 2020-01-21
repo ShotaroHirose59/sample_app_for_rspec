3c0217a6ff7c0d0e953d3a40adbe2f36416005d3
Shotaro Hirose <shotaro199959@icloud.com>
2020-01-21 19:47:34 +0900

    設定を追記しFactoryBotを省略

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe 'validation' do
     it 'titleが空の場合は無効' do
-      task = FactoryBot.build(:task, title: nil)
+      task = build(:task, title: nil)
       task.valid?
       expect(task.errors[:title]).to include("can't be blank")
     end
 
     it '重複したタイトルの場合は無効' do
       FactoryBot.create(:task, title: 'duplicate_task')
-      task = FactoryBot.build(:task, title: 'duplicate_task')
+      task = build(:task, title: 'duplicate_task')
       task.valid?
       expect(task.errors[:title]).to include("has already been taken")
     end
 
     it 'statusが空の場合は無効' do
-      task = FactoryBot.build(:task, status: nil)
+      task = build(:task, status: nil)
       task.valid?
       expect(task.errors[:status]).to include("can't be blank")
     end
