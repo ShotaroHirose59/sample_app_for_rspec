26caeb47bf6db6b17312d6c8732620ac492fb1c1
Shotaro Hirose <shotaro199959@icloud.com>
2020-01-21 19:00:25 +0900

    [Add]/15/model spec

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :task do
-    
+    title { 'テスト'}
+    status { 'todo' }
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'titleが空の場合は無効' do
+      task = FactoryBot.build(:task, title: nil)
+      task.valid?
+      expect(task.errors[:title]).to include("can't be blank")
+    end
+
+    it '重複したタイトルの場合は無効' do
+      FactoryBot.create(:task)
+      task = FactoryBot.build(:task)
+      task.valid?
+      expect(task.errors[:title]).to include("has already been taken")
+    end
+
+    it 'statusが空の場合は無効' do
+      task = FactoryBot.build(:task, status: nil)
+      task.valid?
+      expect(task.errors[:status]).to include("can't be blank")
+    end
+  end
 end
